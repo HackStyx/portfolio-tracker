c6b0fd727c6b2fc78677a6642e3fa47058f2c27b
hackstyx <1bi22cs118@bit-bangalore.edu.in>
2026-05-03 13:55:32 +0530

    fix: escape apostrophe in forgot-password modal tip string

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1830,11 +1830,11 @@ const Homepage = () => {
                       ? 'bg-gray-800/70 border border-gray-700/60'
                       : 'bg-gray-50 border border-gray-100'
                   }`}>
-                    {[
-                      'The link will expire in 24 hours.',
-                      'Check your spam/junk folder if you don't see it.',
-                      'Click the link in the email to set a new password.',
-                    ].map((tip, i) => (
+                      {[
+                        'The link will expire in 24 hours.',
+                        "Check your spam/junk folder if you don't see it.",
+                        'Click the link in the email to set a new password.',
+                      ].map((tip, i) => (
                       <div key={i} className="flex items-start gap-2.5">
                         <svg className="w-4 h-4 mt-0.5 shrink-0 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
                           <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
